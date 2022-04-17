b2e40ba7896cc45cf40caf949cb88770af9afe19
Javier Muñoz Haro <javier.munnozharo@estudiante.uam.es>
2022-04-17 23:24:12 +0200

    pre entrega

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -11,6 +11,12 @@ def get_sheetnames_xlsx(filepath):
 
 if __name__ == '__main__':
     df_unican = pd.read_csv("../rtt_unican.csv")
-    df_vultr = pd.read_csv("../measures/GoogleCloud.csv")
+    print("La varianza calculada para Unican: ", np.var(df_unican['RTT(ms)']))
+
+    for sheet in get_sheetnames_xlsx("../measures.xlsx"):
+        path = "../measures/"+sheet+".csv"
+        df_current = pd.read_csv(path)
+        print("La varianza para ", sheet, ":", np.var(df_current['Mean']))
+    
 
     
